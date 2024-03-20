a160213771b45dd9aa2c8551fbd8e65db0616010
heisetiantang <92718554+heisetiantang@users.noreply.github.com>
2024-03-20 21:34:43 +0800

    稳定版
    调试版，增加RGB地图的代码，但没有启用

--- src/distributedMappingVisualization.cpp
+++ src/distributedMappingVisualization.cpp
@@ -74,29 +74,34 @@ void distributedMapping::publishGlobalMap()
 		*global_map_keyframes += *transformPointCloud(robots[id_].keyframe_cloud_array[pose_6d_tmp.intensity],
 													  &pose_6d_tmp);
 	}
-	if (!robots[id_].keyframe_cloud_rgb_array.empty())
-	{
-		cout << "robots[id_].keyframe_cloud_rgb_array is not empty" << endl;
-		// 仿照、将带颜色的点云拼接
-		pcl::PointCloud<pcl::PointXYZRGB>::Ptr global_map_keyframes_rgb(new pcl::PointCloud<pcl::PointXYZRGB>());
-		pcl::PointCloud<pcl::PointXYZRGB>::Ptr global_map_keyframes_rgb_ds(new pcl::PointCloud<pcl::PointXYZRGB>());
-		for (size_t i = 0; i < (int)indices.size(); ++i)
-		{
-			PointPose6D pose_6d_tmp = poses_6d_cloud_copy->points[indices[i]];
-			*global_map_keyframes_rgb += *transformPointCloud(robots[id_].keyframe_cloud_rgb_array[pose_6d_tmp.intensity],
-															  &pose_6d_tmp);
-		}
+	
+	// if (!robots[id_].keyframe_cloud_rgb_array.empty())
+	// {
+	// 	cout << "robots[id_].keyframe_cloud_rgb_array is not empty" << endl;
+	// 	// 仿照、将带颜色的点云拼接
+	// 	pcl::PointCloud<pcl::PointXYZRGB>::Ptr global_map_keyframes_rgb(new pcl::PointCloud<pcl::PointXYZRGB>());
+	// 	pcl::PointCloud<pcl::PointXYZRGB>::Ptr global_map_keyframes_rgb_ds(new pcl::PointCloud<pcl::PointXYZRGB>());
+	// 	for (size_t i = 0; i < (int)indices.size(); ++i)
+	// 	{
+	// 		PointPose6D pose_6d_tmp = poses_6d_cloud_copy->points[indices[i]];
+	// 		*global_map_keyframes_rgb += *transformPointCloud(robots[id_].keyframe_cloud_rgb_array[pose_6d_tmp.intensity],
+	// 														  &pose_6d_tmp);
+	// 	}
 
-		pcl::VoxelGrid<pcl::PointXYZRGB> downsample_filter_for_global_map_rgb; // for global map visualization
-		downsample_filter_for_global_map_rgb.setLeafSize(map_leaf_size_, map_leaf_size_, map_leaf_size_);
-		downsample_filter_for_global_map_rgb.setInputCloud(global_map_keyframes_rgb);
-		downsample_filter_for_global_map_rgb.filter(*global_map_keyframes_rgb_ds);
-	}
+	// 	pcl::VoxelGrid<pcl::PointXYZRGB> downsample_filter_for_global_map_rgb; // for global map visualization
+	// 	downsample_filter_for_global_map_rgb.setLeafSize(map_leaf_size_, map_leaf_size_, map_leaf_size_);
+	// 	downsample_filter_for_global_map_rgb.setInputCloud(global_map_keyframes_rgb);
+	// 	downsample_filter_for_global_map_rgb.filter(*global_map_keyframes_rgb_ds);
+	// }
 	// 在这里通过接收三个地图和全局地图的初始位姿,对于地图进行初始变换
 	// if (/* condition */)
 	// {
 	// 	/* code */
 	// }
+	// 发布没有降采样的RGB全局地图
+
+
+
 
 	// 发布未进行降采样的全局地图
 	pcl::PointCloud<PointPose3D>::Ptr global_map_keyframes_copy(new pcl::PointCloud<PointPose3D>());
